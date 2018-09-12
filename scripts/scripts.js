5a1a81d90c279ad557bf657adffc925d13d46618
icnpdesign <contact@icnp.design>
2018-09-11 20:41:10 -0400

    Swapped the image and the text on the portfolio cards.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,54 +1,54 @@
-
+const primaryColor = '#db6565';
 
 $(function() {
     $('.card0').hover(function() {
       $('.card0').css('cursor', 'pointer');  
-      $('.btn0').css('background-color', '#f08080');
+      $('.btn0').css('background-color', primaryColor);
       $('.btn0').css('color', '#FFF');
     }, function() {
       // on mouseout, reset the background colour
       $('.btn0').css('background-color', 'inherit');
-      $('.btn0').css('color', '#f08080');
+      $('.btn0').css('color', primaryColor);
     });
 
     $('.card1').hover(function() {
         $('.card1').css('cursor', 'pointer');  
-        $('.btn1').css('background-color', '#f08080');
+        $('.btn1').css('background-color', primaryColor);
         $('.btn1').css('color', '#FFF');
       }, function() {
         // on mouseout, reset the background colour
         $('.btn1').css('background-color', 'inherit');
-        $('.btn1').css('color', '#f08080');
+        $('.btn1').css('color', primaryColor);
       });
 
       $('.card1').hover(function() {
         $('.card1').css('cursor', 'pointer');  
-        $('.btn1').css('background-color', '#f08080');
+        $('.btn1').css('background-color', primaryColor);
         $('.btn1').css('color', '#FFF');
       }, function() {
         // on mouseout, reset the background colour
         $('.btn1').css('background-color', 'inherit');
-        $('.btn1').css('color', '#f08080');
+        $('.btn1').css('color', primaryColor);
       });
 
       $('.card2').hover(function() {
         $('.card2').css('cursor', 'pointer');  
-        $('.btn2').css('background-color', '#f08080');
+        $('.btn2').css('background-color', primaryColor);
         $('.btn2').css('color', '#FFF');
       }, function() {
         // on mouseout, reset the background colour
         $('.btn2').css('background-color', 'inherit');
-        $('.btn2').css('color', '#f08080');
+        $('.btn2').css('color', primaryColor);
       });
 
       $('.card3').hover(function() {
         $('.card3').css('cursor', 'pointer');  
-        $('.btn3').css('background-color', '#f08080');
+        $('.btn3').css('background-color', primaryColor);
         $('.btn3').css('color', '#FFF');
       }, function() {
         // on mouseout, reset the background colour
         $('.btn3').css('background-color', 'inherit');
-        $('.btn3').css('color', '#f08080');
+        $('.btn3').css('color', primaryColor);
       });
     
 
