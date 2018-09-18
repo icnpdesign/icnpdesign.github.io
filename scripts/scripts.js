358065fa8f480b73486e471ea15e4038dbb79151
icnpdesign <contact@icnp.design>
2018-09-18 11:30:50 -0400

    Added a footer across all pages, fixed the cards, made the social media icons into a ul

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,28 @@
 const primaryColor = '#db6565';
+let cards = ['.card0', '.card1', '.card2', '.card3', '.card4', '.card5']
+let buttons = ['.btn0', '.btn1', '.btn2', '.btn3', '.btn4', '.btn5']
 
-$(function() {
+ $(document).ready(function(){
+  for (i = 0; i < cards.length; i++) {
+    $(function() {
+      $(cards[i]).hover(function() {
+        $(cards[i]).css('cursor', 'pointer');  
+        $(buttons[i]).css('background-color', primaryColor);
+        $((buttons[i]).css('color', '#FFF');
+      }, function() {
+        // on mouseout, reset the background colour
+        $((buttons[i]).css('background-color', 'inherit');
+        $((buttons[i]).css('color', primaryColor);
+      });
+  })}
+
+  /* $(function() {
     $('.card0').hover(function() {
       $('.card0').css('cursor', 'pointer');  
       $('.btn0').css('background-color', primaryColor);
       $('.btn0').css('color', '#FFF');
     }, function() {
-      // on mouseout, reset the background colour
+      //on mouseout, reset the background colour
       $('.btn0').css('background-color', 'inherit');
       $('.btn0').css('color', primaryColor);
     });
@@ -16,7 +32,7 @@ $(function() {
         $('.btn1').css('background-color', primaryColor);
         $('.btn1').css('color', '#FFF');
       }, function() {
-        // on mouseout, reset the background colour
+        //on mouseout, reset the background colour
         $('.btn1').css('background-color', 'inherit');
         $('.btn1').css('color', primaryColor);
       });
@@ -26,7 +42,7 @@ $(function() {
         $('.btn1').css('background-color', primaryColor);
         $('.btn1').css('color', '#FFF');
       }, function() {
-        // on mouseout, reset the background colour
+        //on mouseout, reset the background colour
         $('.btn1').css('background-color', 'inherit');
         $('.btn1').css('color', primaryColor);
       });
@@ -36,7 +52,7 @@ $(function() {
         $('.btn2').css('background-color', primaryColor);
         $('.btn2').css('color', '#FFF');
       }, function() {
-        // on mouseout, reset the background colour
+        //on mouseout, reset the background colour
         $('.btn2').css('background-color', 'inherit');
         $('.btn2').css('color', primaryColor);
       });
@@ -46,20 +62,83 @@ $(function() {
         $('.btn3').css('background-color', primaryColor);
         $('.btn3').css('color', '#FFF');
       }, function() {
-        // on mouseout, reset the background colour
+        //on mouseout, reset the background colour
         $('.btn3').css('background-color', 'inherit');
         $('.btn3').css('color', primaryColor);
       });
+
+      $('.card4').hover(function() {
+        $('.card4').css('cursor', 'pointer');  
+        $('.btn4').css('background-color', primaryColor);
+        $('.btn4').css('color', '#FFF');
+      }, function() {
+        //on mouseout, reset the background colour
+        $('.btn4').css('background-color', 'inherit');
+        $('.btn4').css('color', primaryColor);
+      });
     
-
-      $(function() {
-        var path = window.location.pathname.substring(1);
-        $('.nav>li>a[href="' + path + '"]').parent().addClass('active');
-
-    });
-
+      $('.card5').hover(function() {
+        $('.card5').css('cursor', 'pointer');  
+        $('.btn5').css('background-color', primaryColor);
+        $('.btn5').css('color', '#FFF');
+      }, function() {
+        //on mouseout, reset the background colour
+        $('.btn5').css('background-color', 'inherit');
+        $('.btn5').css('color', primaryColor);
+      });
+    }); */
     
+    var getMax = function(){
+        return $(document).height() - $(window).height();
+    }
+    
+    var getValue = function(){
+        return $(window).scrollTop();
+    }
+    
+    if('max' in document.createElement('progress')){
+        // Browser supports progress element
+        var progressBar = $('progress');
+        
+        // Set the Max attr for the first time
+        progressBar.attr({ max: getMax() });
+
+        $(document).on('scroll', function(){
+            // On scroll only Value attr needs to be calculated
+            progressBar.attr({ value: getValue() });
+        });
+      
+        $(window).resize(function(){
+            // On resize, both Max/Value attr needs to be calculated
+            progressBar.attr({ max: getMax(), value: getValue() });
+        });   
+    }
+    else {
+        var progressBar = $('.progress-bar'), 
+            max = getMax(), 
+            value, width;
+        
+        var getWidth = function(){
+            // Calculate width in percentage
+            value = getValue();            
+            width = (value/max) * 100;
+            width = width + '%';
+            return width;
+        }
+        
+        var setWidth = function(){
+            progressBar.css({ width: getWidth() });
+        }
+        
+        $(document).on('scroll', setWidth);
+        $(window).on('resize', function(){
+            // Need to reset the Max attr
+            max = getMax();
+            setWidth();
+        });
+    }
+});
 
 
-  });
+
 
